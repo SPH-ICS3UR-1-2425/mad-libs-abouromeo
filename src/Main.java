7893a1946bec28be8c0a4640a0344dcec699d758
abouromeo <tsinda.romeo@stu.ocsb.ca>
2024-09-18 14:41:22 -0400

    change 23

--- src/Main.java
+++ src/Main.java
@@ -16,14 +16,31 @@ public class Main {
 		System.out.println("Welcome to Mad Libs!");
 		System.out.println("What's your name?");
 		String name = in.nextLine();
-		System.out.println("Give me a number: ");
-		int num1 = in.nextInt();
-		System.out.println("GIve me a place: ");
+		System.out.println("How old are you?");
+	    int num1 = in.nextInt();
+		System.out.println("What is your father's name?");
 		// flush
 		in. nextLine();
-		String place = in.nextLine();
-		
-		System.out.println("One day, "+name+" wrote "+num1+" a Mad Lib in "+place+" ");
+		String name2 = in.nextLine();
+		System.out.println("what is the name of your mother?");
+		String name3 = in.nextLine();
+		System.out.println("How old was your father when you were born?");
+	    int num2 = in.nextInt();
+	    System.out.println("How old was your mother when you were born?");
+	    int num3 = in.nextInt();
+	    System.out.println("How many siblings do you have?" );
+	    int num4 = in.nextInt();
+	    System.out.println("give me a place");
+	    in.nextLine();
+	    String name4 = in.nextLine();
+	    System.out.println("give me your sibling's name");
+	    String name5 = in.nextLine();
+	    System.out.println("give me a year ");
+	    int num5 = in.nextInt()
+;
+	   
+	    
+		System.out.println("One day, "+name+" wrote "+num1+" a Mad Lib "+num2+" in");
 	}
 
 }
