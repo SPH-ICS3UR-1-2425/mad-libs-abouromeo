373e9ede9c519460a39778af1061322dad8c2ed3
abouromeo <tsinda.romeo@stu.ocsb.ca>
2024-09-19 14:39:06 -0400

    new stuff

--- src/Main.java
+++ src/Main.java
@@ -18,29 +18,26 @@ public class Main {
 		String name = in.nextLine();
 		System.out.println("How old are you?");
 	    int num1 = in.nextInt();
-		System.out.println("What is your father's name?");
+		System.out.println("give me a PET name?");
 		// flush
 		in. nextLine();
 		String name2 = in.nextLine();
-		System.out.println("what is the name of your mother?");
+		System.out.println("what is your favourite snack?");
 		String name3 = in.nextLine();
-		System.out.println("How old was your father when you were born?");
-	    int num2 = in.nextInt();
-	    System.out.println("How old was your mother when you were born?");
+		System.out.println("What is your hobby?");
+	    String name6 = in.nextLine();
+	    System.out.println("How many hours do you sleep ?");
 	    int num3 = in.nextInt();
-	    System.out.println("How many siblings do you have?" );
+	    System.out.println("How many cars do you have?" );
 	    int num4 = in.nextInt();
 	    System.out.println("give me a place");
 	    in.nextLine();
 	    String name4 = in.nextLine();
-	    System.out.println("give me your sibling's name");
+	    System.out.println("give me a colour");
 	    String name5 = in.nextLine();
 	    System.out.println("give me a year ");
-	    int num5 = in.nextInt()
-;
-	   
-	    
-		System.out.println("One day, "+name+" wrote "+num1+" a Mad Lib "+num2+" in");
-	}
+	    int num5 = in.nextInt();
 
+	   	System.out.println("One day, "+name+" was sitting with "+name5+" eating "+name4+"  "+name6+"years  in");
+	
 }
